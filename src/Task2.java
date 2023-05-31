58d6ee7c38a9b33b2da6fcc2c140014942f337c1
Wladimir <weizen.w@gmx.de>
2023-05-31 14:03:39 +0200

    feat: add Task2 solution

--- src/Task2.java
+++ src/Task2.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task2 {
 
   public static void main(String[] args) {
@@ -6,7 +10,23 @@ public class Task2 {
 //    Сохранить эти числа в множестве.
 //    Удалить из множества все нечётные числа.
 //    Вывести получившееся множество.
-
+    Scanner scanner = new Scanner(System.in);
+    Set<Integer> numbers = new HashSet<>();
+    System.out.print("Введите кол-во чисел для записи: ");
+    int n = scanner.nextInt();
+    for (int i = 0; i < n; ++i) {
+      System.out.print("Введите " + (i + 1) + "-ое число: ");
+      int x = scanner.nextInt();
+      numbers.add(x);
+    }
+    System.out.println("Записанные числа в множестве: " + numbers);
+    Set<Integer> numbersToRemove = new HashSet<>();
+    for (int x : numbers) {
+      if (x % 2 != 0) {
+        numbersToRemove.add(x);
+      }
+    }
+    numbers.removeAll(numbersToRemove);
+    System.out.println("После удаления нечетных чисел: " + numbers);
   }
-
 }
